f93dfbbde253de3fd56829cb62b308ad1973e1f7
Nicholas Ng <nicholasykng@gmail.com>
2019-10-23 02:50:21 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name=(dog_name)
+  @this_dog_name = dog_name
+  end
+  def name
+    @this_dog_name
+  end
+  def breed=(dog_breed)
+    @this_dog_breed = dog_breed
+  end
+  def breed
+    @this_dog_breed
+  end
+end
